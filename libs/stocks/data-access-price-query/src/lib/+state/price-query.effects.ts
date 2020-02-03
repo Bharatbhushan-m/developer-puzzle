1dfc272b05ac2b0d89c063d8e4586f303f98bf76
Bharat Mishra <bharat.mishra5@t-mobile.com>
2020-02-04 00:15:10 +0530

    Task 3 completed with date range

--- libs/stocks/data-access-price-query/src/lib/+state/price-query.effects.ts
+++ libs/stocks/data-access-price-query/src/lib/+state/price-query.effects.ts
@@ -6,7 +6,7 @@ import {
 } from '@coding-challenge/stocks/data-access-app-config';
 import { Effect } from '@ngrx/effects';
 import { DataPersistence } from '@nrwl/nx';
-import { map } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 import {
   FetchPriceQuery,
   PriceQueryActionTypes,
@@ -29,8 +29,18 @@ export class PriceQueryEffects {
             }?token=${this.env.apiKey}`
           )
           .pipe(
-            map(resp => new PriceQueryFetched(resp as PriceQueryResponse[]))
-          );
+            map((resp: PriceQueryResponse[]) =>
+            resp.filter(priceQuery => {
+                const respDate = new Date(priceQuery.date + ' 0:0:0');
+                const minDate = action.fromDate.getTime();
+                const maxDate = action.toDate.getTime();
+                return (respDate.getTime() >= minDate && respDate.getTime() <= maxDate)
+              })), switchMap(resp => {
+                return [
+                  new PriceQueryFetched(resp as PriceQueryResponse[]),
+                ];
+              })
+            )
       },
 
       onError: (action: FetchPriceQuery, error) => {
--- libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
+++ libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PriceQueryFacade } from '@coding-challenge/stocks/data-access-price-query';
 import { Subscription } from 'rxjs';
+import { MatDatepickerInputEvent } from '@angular/material';
 
 @Component({
   selector: 'coding-challenge-stocks',
@@ -11,35 +12,22 @@ import { Subscription } from 'rxjs';
 export class StocksComponent implements OnInit {
   stockPickerForm: FormGroup;
   symbol: string;
+  maxDate: Date = new Date();
   period: string;
 
   quotes$ = this.priceQuery.priceQueries$;
 
   private stockPickerValueChangeSubscription: Subscription;
 
-  timePeriods = [
-    { viewValue: 'All available data', value: 'max' },
-    { viewValue: 'Five years', value: '5y' },
-    { viewValue: 'Two years', value: '2y' },
-    { viewValue: 'One year', value: '1y' },
-    { viewValue: 'Year-to-date', value: 'ytd' },
-    { viewValue: 'Six months', value: '6m' },
-    { viewValue: 'Three months', value: '3m' },
-    { viewValue: 'One month', value: '1m' }
-  ];
-
   constructor(private fb: FormBuilder, private priceQuery: PriceQueryFacade) {
-  }
-
-  ngOnInit(): void {
-    this.stockPickerForm = this.fb.group({
+    this.stockPickerForm = fb.group({
       symbol: [null, Validators.required],
-      period: [null, Validators.required]
+      fromDate: [null, Validators.required],
+      toDate: [null, Validators.required]
     });
-
-    this.stockPickerValueChangeSubscription = this.stockPickerForm.valueChanges
-      .subscribe(() => this.fetchQuote());
   }
+
+  ngOnInit(): void {}
 
   ngOnDestroy(): void {
     this.stockPickerValueChangeSubscription.unsubscribe();
@@ -48,8 +36,29 @@ export class StocksComponent implements OnInit {
 
   fetchQuote() {
     if (this.stockPickerForm.valid) {
-      const { symbol, period } = this.stockPickerForm.value;
-      this.priceQuery.fetchQuote(symbol, period);
+      const { symbol, fromDate, toDate } = this.stockPickerForm.value;
+      this.period = 'max';
+      this.priceQuery.fetchQuote(symbol, this.period, fromDate, toDate);
+    }
+  }
+
+  /*if from date is after to date, make it the same as to date*/
+  public fromDateChange(date): void {
+    const { toDate } = (this.stockPickerForm.get('toDate').valid) ? this.stockPickerForm.value : '';
+    if (toDate) {
+      if (date.value.getTime() > toDate.getTime()) {
+        this.stockPickerForm.controls.fromDate.setValue(toDate)
+      };
+    }
+  }
+
+  /*if to date is after from date, make it the same as from date*/
+  public toDateChange(date): void {
+    const { fromDate } = (this.stockPickerForm.get('fromDate').valid) ? this.stockPickerForm.value : '';
+    if (fromDate) {
+      if (date.value.getTime() < fromDate.getTime()) {
+        this.stockPickerForm.controls.toDate.setValue(fromDate)
+      }
     }
   }
 }
