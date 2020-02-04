2cc802e8ff54f321cc9cf7e767d11da5711c3912
Bharat Mishra <bharat.mishra5@t-mobile.com>
2020-02-04 11:40:54 +0530

    Task 2 test case completed

--- libs/stocks/feature-shell/src/lib/stocks/stocks.component.spec.ts
+++ libs/stocks/feature-shell/src/lib/stocks/stocks.component.spec.ts
@@ -1,6 +1,12 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
 
 import { StocksComponent } from './stocks.component';
+import { PriceQueryFacade } from '@coding-challenge/stocks/data-access-price-query';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule, MatInputModule, MatSelectModule } from '@angular/material';
+import { SharedUiChartModule } from '@coding-challenge/shared/ui/chart';
+import { of } from 'rxjs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('StocksComponent', () => {
   let component: StocksComponent;
@@ -8,7 +14,23 @@ describe('StocksComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ StocksComponent ]
+      declarations: [ StocksComponent ],
+      providers: [
+        {
+          provide: PriceQueryFacade, useValue: {
+            priceQueries$: of([]),
+            fetchQuote: jest.fn()
+          }
+        }
+      ],
+      imports: [
+        BrowserAnimationsModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        SharedUiChartModule
+      ],
     })
     .compileComponents();
   }));
@@ -19,7 +41,46 @@ describe('StocksComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('Initialization', () => {
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+  });
+
+  describe('detectChanges', () => {
+    it('should not process quotes if only symbol was provided', fakeAsync(() => {
+      component.stockPickerForm.patchValue({
+        symbol: 'AAPL'
+      }, {
+          emitEvent: true
+        });
+      fixture.detectChanges();
+      const priceQueryFacade: PriceQueryFacade = TestBed.get(PriceQueryFacade);
+      expect(priceQueryFacade.fetchQuote).not.toHaveBeenCalled();
+    }));
+
+    it('should not process quotes if only period was provided', fakeAsync(() => {
+      component.stockPickerForm.patchValue({
+        period: '1y'
+      }, {
+          emitEvent: true
+        });
+      fixture.detectChanges();
+      const priceQueryFacade: PriceQueryFacade = TestBed.get(PriceQueryFacade);
+      expect(priceQueryFacade.fetchQuote).not.toHaveBeenCalled();
+    }));
+
+    it('should request quotes and get response once form is filled and valid', fakeAsync(() => {
+      component.stockPickerForm.patchValue({
+        symbol: 'AAPL',
+        period: '2y'
+      }, {
+          emitEvent: true
+        });
+      fixture.detectChanges();
+      const priceQueryFacade: PriceQueryFacade = TestBed.get(PriceQueryFacade);
+      expect(priceQueryFacade.fetchQuote).toHaveBeenCalledTimes(1);
+      expect(priceQueryFacade.fetchQuote).toHaveBeenCalledWith('AAPL', '2y');
+    }));
   });
 });
